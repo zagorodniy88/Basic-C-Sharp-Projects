5d6cd06a1de3c4873b26dee69302bbc872f075ef
Kris <krisgodom@gmail.com>
2021-12-15 13:12:18 -0400

    first commit

---
+++ Assignments/Assignment1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems\nRemember, we’re \"accounting\" on you!");
+            Console.Read();
+        }
+    }
+}

0ef9fd3bbdbf631350a2bb0a476d3762dbfa9bb4
Alex <zagorodniy88@gmail.com>
2021-12-21 19:17:12 -0400

    add 4 assignments

---
+++ LoopAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LoopAssignment
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter something to the program. Or type 'stop' to quit.");
+
+            string input = "";
+
+            while (input != "stop")
+            {
+                input = Console.ReadLine();
+                Console.WriteLine("You entered: " + input);
+               
+            }
+
+            Console.WriteLine("Enter something again to the program. Or type 'stop' to quit.");
+
+            do
+            {
+                input = Console.ReadLine();
+                Console.WriteLine("You entered: " + input);
+            }
+            while (input != "stop");
+        }
+    }
+}
---
+++ StringAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using System.Text;
+
+
+namespace StringAssignment
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("In a console app, create code that does the following:\n");
+            stringBuilder.Append("1. Concatenate three strings.\n");
+            stringBuilder.Append("2. Convert a string to uppercase.\n");
+            stringBuilder.Append("3. Create a StringBuilder and build a paragraph of text, one sentence at a time.\n");
+            Console.WriteLine(stringBuilder.ToString());
+
+            string str1 = "I'm ";
+            string str2 = "learning ";
+            string str3 = "programming";
+            Console.WriteLine(str1 + str2 + str3);
+            Console.WriteLine(str3.ToUpper());
+
+            Console.ReadLine();
+
+        }
+    }
+}
